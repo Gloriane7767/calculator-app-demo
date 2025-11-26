41a3d9953ada56fdb6ae2df2bf92cbaa5cfeb1d2
Glory Ngum Foncha <gloriane7767@gmail.com>
2025-11-26 09:44:50 +0100

    add scanner and input sections.

---
+++ src/main/java/com/gloriane/CalculatorApp.java
@@ -0,0 +1,21 @@
+package com.gloriane;
+import java.util.Scanner;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter second number:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Select operation (+, -, *, /):");
+        String operation = scanner.next();
+
+        double result = 0;
+
+
+    }
+}
